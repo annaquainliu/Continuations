c53af84c11aa5ec33f0235ab2a0f0dc34ad8ac0b
annaquainliu <annaquainliu@gmail.com>
2024-01-23 18:14:18 -0500

    changes

--- main.js
+++ main.js
@@ -3,5 +3,44 @@ import {SolveSat, Not, And, Or, Symbol} from "./scripts/continuations.js";
 window.onload = () => {
     const body = document.getElementById("solution")
     const SatSolver = new SolveSat();
-    body.innerText = JSON.stringify(SatSolver.solve(new Symbol("x"), () => "FAIL", (cur, resume) => cur));
+    const button = document.querySelector("button");
+    const input = document.getElementById("formula");
+
+    button.addEventListener("click", () => {
+        let formula = parseInput(input.value);
+    })
+}
+/**
+ * 
+ * @param {String} input 
+ * @returns {BooleanFormula}
+ */
+function parseInput(input) {
+    let fields = input.split(" ");
+    let queue = [];
+    let stack = [];
+    for (let field in fields) {
+        if (field == "" || field == null) {
+            continue;
+        }
+        queue.push(field);
+    }
+    queue.reverse();
+    // x && y && x && t || z
+    while (queue.length != 0) {
+        let front = queue.pop();
+        if (front == "!") {
+            let bool = queue.pop();
+            stack.push(new Not())
+        } 
+        else if (front == "&&") {
+
+        }  
+        else if (front == "||") {
+
+        } 
+        else {
+
+        }
+    }
 }
